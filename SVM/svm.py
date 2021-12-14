d6ab938dc4684c3f94e3bc62338d1e4d14cc1ee3
Bernardo Hummes <hummescmpa@gmail.com>
2021-12-14 09:05:22 +0100

    started svm

--- SVM/svm.py
+++ SVM/svm.py
@@ -11,8 +11,31 @@ DATA_DIR = "../data"
 FEAT_TRAIN_FILE = "features_training.csv"
 FEAT_TEST_FILE = "features_testing.csv"
 
-def load_parameters(filename):
-    pass
+
+def load_data(filename):
+    data_path = os.path.join(DATA_DIR, filename)
+    data_df = pd.read_csv(data_path, sep=',', header=0)
+
+    Y = data_df.Style.values
+
+    features = data_df.columns.values[:-2]
+    data_df = data_df.drop('Style', axis=1)
+    data_df = data_df.drop('FileName', axis=1)
+
+    X = data_df.values
+
+    return X, Y
+
 
 if __name__ == "__main__":
-    feat_train_path = os.path.join(DATA_DIR, FEAT_TRAIN_FILE)
+    # load data
+    X_train, Y_trian = load_data(FEAT_TRAIN_FILE)
+    X_test, Y_test = load_data(FEAT_TEST_FILE)
+
+    print(X_train)
+
+    # reduce dimension of features
+
+    # wat
+    svm = LinearSVC(penalty='l2', loss='squared_hinge', dual=False,
+                    multi_class='crammer_singer', )
