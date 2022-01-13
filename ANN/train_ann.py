c0c3b18018c5afbbe24930f72cb7762c237e5391
Bernardo Hummes <hummescmpa@gmail.com>
2022-01-13 19:14:13 +0100

    saving ann model to text file

--- ANN/train_ann.py
+++ ANN/train_ann.py
@@ -13,6 +13,9 @@ nb_epochs = 100
 
 DATA_DIR = "./DATA"
 FEAT_TRAIN_FILE = "features_training.csv"
+FEAT_TEST_FILE = "features_testing.csv"
+ANN_WEIGHTS_FILE = "ann_weights.txt"
+
 
 def load_data(filename):
     data_path = os.path.join(DATA_DIR, filename)
@@ -28,9 +31,10 @@ def load_data(filename):
     X = data_df.values
 
     return X, y, classes
-    
-def model_generator(X):  
-    model=tf.keras.Sequential()
+
+
+def model_generator(X):
+    model = tf.keras.Sequential()
     model.add(tf.keras.layers.Dense(256, activation='relu', input_dim=X.shape[1]))
     model.add(tf.keras.layers.Dense(64, activation='relu'))
     model.add(tf.keras.layers.Dense(nb_classes, activation='softmax'))
@@ -38,18 +42,51 @@ def model_generator(X):
 
 
 if __name__ == "__main__":
-    X, Y, classes = load_data(FEAT_TRAIN_FILE) #features, label, vector of the classes
-    unq,tags = np.unique(Y, return_inverse=True)
-    dict(zip(range(len(unq)),unq))
-    Y=tags
+    X_train, Y_train, classes = load_data(FEAT_TRAIN_FILE)  # features, label, vector of the classes
+    X_test, Y_test, _ = load_data(FEAT_TEST_FILE)
+
+    unq, tags = np.unique(Y_train, return_inverse=True)
+    Y_train = tags
+
+    unq, tags = np.unique(Y_test, return_inverse=True)
+    Y_test = tags
+
     #Y = encName2Ind.transform(Y)
-    print(Y)
+    print(Y_train)
+
     #ds_train = tf.data.Dataset.from_tensor_slices((X, Y))
 
-    model = model_generator(X)
-    #model.summary()
+    model = model_generator(X_train)
+    model.summary()
+
     #optimizer = keras.optimizers.SGD(learning_rate=0.001, momentum=0.9, decay=0.01)
     optimizer = keras.optimizers.RMSprop(0.0001)
-    model.compile(loss="sparse_categorical_crossentropy", optimizer=optimizer,
-                  metrics=["accuracy"])
-    history = model.fit(X, Y, epochs=nb_epochs, validation_split = 0.4)
+
+    model.compile(loss="sparse_categorical_crossentropy",
+                  optimizer=optimizer,
+                  metrics=["accuracy"]
+                  )
+
+    history = model.fit(X_train, Y_train, epochs=nb_epochs, validation_split=0.4)
+
+    print("Generating weights file")
+    with open(os.path.join(DATA_DIR, ANN_WEIGHTS_FILE), "w+") as f_ann:
+
+        f_ann.write("{}\n".format(len(model.layers)))  # first line shows how many layers are there to read
+        # export the trained weights
+        for i,layer in enumerate(model.layers):
+            print("Layer", i)
+
+            weights = layer.get_weights()[0]
+            bias = layer.get_weights()[1]
+
+            # start each layer with the shape of the matrix representing it
+            # always assuming last column will be composed of the bias
+            f_ann.write("{} {}\n".format(weights.shape[0], weights.shape[1]+1))
+
+            for w,b in zip(weights,bias):
+                # store the matrix line by line
+                line = ",".join(map(str,w)) + ",{}\n".format(b)
+                f_ann.write(line)
+
+    print("Finished saving model")
